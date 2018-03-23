a48852a93abce92423bec501092ce0bcddc4631d
marclevetin <marc@derbyhat.org>
2018-03-23 12:22:25 -0400

    summary commit of work done March 22 2018

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var elem = document.querySelector('.sidenav');
+var instance = M.Sidenav.init(elem);
+
+var elem2 = document.querySelector('.collapsible');
+var instance2 = M.Collapsible.init(elem2);
