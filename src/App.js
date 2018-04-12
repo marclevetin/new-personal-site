cf597ff6188f328c165ff5cd080b58753c1a19b9
marclevetin <marc@derbyhat.org>
2018-04-12 16:24:35 -0400

    added material ui npm package.  Added material UI icons npm package.  Set up Navbar and CSS baseline

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// material-ui
+import CssBaseline from "material-ui/CssBaseline";
+import NavBar from './Navbar';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <React.Fragment>
+        <CssBaseline />
+        <NavBar/>
+        Test
+      </React.Fragment>;
   }
 }
 
