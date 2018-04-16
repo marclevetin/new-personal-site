ed3a0f4db64b3d766ac416c1006c202c06a162d4
marclevetin <marc@derbyhat.org>
2018-04-16 12:14:29 -0400

    added initial project data

---
+++ src/data/projects.js
@@ -0,0 +1,65 @@
+const projects = [
+  {
+    id: 1,
+    project: "Class Superlatives",
+    liveurl: "https://unh-best-of-bootcamp.herokuapp.com/",
+    codeurl: "https://github.com/marclevetin/class-superlatives",
+    imageurl: "",
+    description:
+      "A virtual Yearbook for UNH Coding Bootcamp.  React, Mongo, Bootstrap 3"
+  }
+  {
+    id: 2,
+    project: "Portsmouth Randomizer",
+    liveurl: "https://portsmouth-randomizer.herokuapp.com/",
+    codeurl: "https://github.com/marclevetin/portsmouth-randomizer",
+    imageurl: "",
+    description:
+      "A collection of tools that make running the UNH Coding Bootcamp simpler.  React, Ruby, Sinatra"
+  },
+  {
+    id: 3,
+    project: "My Ukulele Info",
+    liveurl: "https://my-ukulele-info.herokuapp.com/",
+    codeurl: "https://github.com/marclevetin/myukuleleinfo",
+    imageurl: "",
+    description:
+      "Tracks ukulele collections online with pictures, instrument details, and video.  Exports collection to a popular Ukulele bulletin board.  React, Ruby on Rails, AWS S3"
+  },
+    {
+    id: 4,
+    project: "Dad Jokes",
+    liveurl: "https://marclevetin.github.io/dad-jokes/",
+    codeurl: "https://github.com/marclevetin/dad-jokes",
+    imageurl: "",
+    description:
+      "Get a random Dad Joke from an API with a click of a button or a search.  React, Materialize CSS"
+  },
+  {
+    id: 5,
+    project: "Hangman",
+    liveurl: "https://marclevetin.github.io/hangman-game/",
+    codeurl: "https://github.com/marclevetin/hangman-game",
+    imageurl: "",
+    description:
+      "Guess the word before you spell HANGMAN.  Javascript"
+  },
+  {
+    id: 6,
+    project: "Twitter Bot",
+    liveurl: "#",
+    codeurl: "https://github.com/marclevetin/twitter-bot-node",
+    imageurl: "",
+    description:
+      "Node app that tweets out a random dad joke.  Node, Twitter, Heroku Scheduler"
+  },
+  {
+    id: 7,
+    project: "Disney API CLI",
+    liveurl: "https://portsmouth-randomizer.herokuapp.com/",
+    codeurl: "https://github.com/marclevetin/portsmouth-randomizer",
+    imageurl: "",
+    description:
+      "Node app to get the weather and wait times at Walt Disney World.  Node, Inquirer, Themeparks API, Weather JS"
+  }
+];
